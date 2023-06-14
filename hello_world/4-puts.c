605451195975380935bcd810c6d600ad41377c7a
root <6769@holbertonstudents.com>
2023-06-14 10:55:12 -0700

    task 4 Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,2 @@
 #include <stdio.h>
-/*
- int main(){
-        main(void);
-
-	puts(); //"Programming is like building a multilingual puzzle" 
-
-	return 0;
-}
-*/
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+int main(void){puts("\"Programming is like building a multilingual puzzle");return (0);}
