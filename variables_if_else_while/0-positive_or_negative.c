23a9dd312e0db5d4769cbecf9e729798b6d17100
root <6769@holbertonstudents.com>
2023-06-15 07:08:20 -0700

    task 0 This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "This program will assign a random number to the variable n each time it is executed. Complete the source code
- * in order to print whether the number stored in the variable n is positive or negative."
+ * main - Prints if number is positive, zero or negative
  *
  *
  * return always 0.
