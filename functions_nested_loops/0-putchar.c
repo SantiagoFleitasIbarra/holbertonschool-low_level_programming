92fef9d27a2ad8685b92f39e9ca021b87a82e869
root <6769@holbertonstudents.com>
2023-06-16 05:09:29 -0700

    task 0 Write a program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Write a program that prints _putchar, followed by a new line.
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
