f2baa35812c5b9b146cb13e56646459f9a14d9db
root <6769@holbertonstudents.com>
2023-06-22 09:07:12 -0700

    task 7 Write a function that prints half of a string, followed by a new line.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,18 @@
-#include "main.h"
 #include <string.h>
 #include <stdio.h>
-/***/
+#include "main.h"
+/**
+ * puts_half - Write a function that prints half of a string, followed by a new
+ * line.
+ * @str: string
+ *
+*/
 void puts_half(char *str)
 {
 	int len = strlen(str);
+
 	int i;
+
 	for (i = len / 2; i < len; i++)
 	{
 		printf("%d", i);
