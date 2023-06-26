999500596b9b50a932084e0a0960012ecc6b7ff2
root <6769@holbertonstudents.com>
2023-06-26 08:21:21 -0700

    task 7 Write a function that encodes a string into 1337.

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ * @str: string
+ *
+ * Return: Always str (Success)
+*/
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
