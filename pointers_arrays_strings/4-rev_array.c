3fba4f566df3c1c436c458de1c805b7a36bba4c1
root <6769@holbertonstudents.com>
2023-06-26 07:07:00 -0700

    task 4 Write a function that reverses the content of an array of integers.

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: integer
+ * @n: is the number of elements of the array
+ *
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	j = n - 1
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		j--;
+	}
+}
