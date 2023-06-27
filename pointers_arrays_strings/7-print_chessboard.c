2aa323873dae7bec056cc9f9e52d757e67a3a166
root <6769@holbertonstudents.com>
2023-06-27 14:24:23 -0700

    task 6 Write a function that prints the chessboard.

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: pointer with the location of the string
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
