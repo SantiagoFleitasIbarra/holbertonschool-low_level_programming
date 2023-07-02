cf52ba96f446a150695c43249b61efe3fadea134
root <6769@holbertonstudents.com>
2023-07-02 15:44:19 -0700

    task 1 Write a program that prints the number of arguments passed into it.

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
