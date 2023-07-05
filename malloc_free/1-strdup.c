c72d39fa63280db3ee865b5eac99a3da78667a92
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-05 09:22:10 -0700

    task 1 Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+# include "main.h"
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in memo
+ * ry, which contains a copy of the string given as a parameter.
+ * @str: string
+ * Return: Always t (Success)
+*/
+char *_strdup(char *str)
+{
+	int i;
+	int c = 0;
+
+	char *t;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+	t = malloc(sizeof(char) * c + 1);
+
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < c; i++)
+	{
+		t[i] = str[i];
+	}
+	return (t);
+}
