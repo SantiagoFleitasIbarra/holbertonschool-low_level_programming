ff2e0d0e577594e645b21396b6090912e08be886
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-06 09:19:02 -0700

    task 1 Write a function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,47 +14,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	len1 = 0;
 	len2 = 0;
 
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
 	if (n >= len2)
-	{
 		n = len2;
-	}
 
 	s = malloc(sizeof(char) * n + len1 + 1);
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		s[i] = s1[i];
-	}
 
 	for (i = 0; i < n; i++)
-	{
 		s[i + len1] = s2[i];
-	}
 
 	s[i + len1] = '\0';
 
