b0ae70374d1940578eecde858c3516edb0e773d0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-11 06:36:18 -0700

    task 4 Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,13 @@
 # include "dog.h"
 # include <stdio.h>
 # include <stdlib.h>
-/***/
+/**
+ * new_dog - a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: Always dog (Success)
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, len1, len2;
@@ -10,19 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = 0;
 	for (i = 0; name[i] != '\0'; i++)
-	{
 		len1++;
-	}
 	len2 = 0;
 	for (i = 0; owner[i] != '\0'; i++)
-	{
 		len2++;
-	}
 	sdog = malloc(sizeof(dog_t));
 	if (sdog == NULL)
-	{
 		return (NULL);
-	}
 	a = malloc((len1 + 1) * sizeof(char));
 	if (a == NULL)
 	{
@@ -30,23 +30,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < len1; i++)
-	{
 		a[i] = name[i];
-	}
 	a[i] = '\0';
 	sdog->name = a;
 	sdog->age = age;
 	b = malloc((len2 + 1) * sizeof(char));
+
 	if (b == NULL)
 	{
 		free(a);
 		free(sdog);
 		return (NULL);
 	}
+
 	for (i = 0; i < len2; i++)
-	{
 		b[i] = owner[i];
-	}
 	b[i] = '\0';
 	sdog->owner = b;
 	return (sdog);
