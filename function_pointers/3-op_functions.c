56697119ce7f4a7cc3fc24441ebd7145b98dcc7b
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-12 07:36:15 -0700

    task 3 This file should contain the 5 following functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  * @b: number
  * Return: Always add (Success)
 */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }
@@ -27,7 +27,7 @@ int op_sub(int a, int b)
  * @b: number
  * Return: Always mul (Success)
 */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
