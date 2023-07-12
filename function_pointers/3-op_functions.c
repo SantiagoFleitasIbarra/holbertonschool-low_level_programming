aca3fad0b23c17742863bd90d6df89092a79b0cb
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-12 07:03:47 -0700

    task 3 This file should contain the 5 following functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+# include "3-calc.h"
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * op_add - op add
+ * @a: number
+ * @b: number
+ * Return: Always add (Success)
+*/
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+/**
+ * op_sub - op sub
+ * @a: number
+ * @b: number
+ * Return: Always sub (Success)
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - op mul
+ * @a: number
+ * @b: number
+ * Return: Always mul (Success)
+*/
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - op div
+ * @a: number
+ * @b: number
+ * Return: Always div (Success)
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - op mod
+ * @a: number
+ * @b: number
+ * Return: Always mod (Success)
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
