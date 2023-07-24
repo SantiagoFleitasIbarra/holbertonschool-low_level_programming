0220a1bd47d29974b67c572da83c7f05c2f2e765
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-24 08:18:44 -0700

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 # ifndef _LISTS_H_
-# define _LIST_H_
+# define _LISTS_H_
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
@@ -13,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
