c87a385bcf43fa4ef39c650c4ed67cc646f2cff7
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-24 07:44:09 -0700

    added library"

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 # define _LIST_H_
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,4 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
 # endif
