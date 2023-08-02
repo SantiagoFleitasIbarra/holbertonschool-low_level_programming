21520a1818a4e067f4e54aa146e45336eb7569f6
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-02 08:04:28 -0700

    task 0 Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+# include "main.h"
+/***/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, l, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	l = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, l);
+
+	close(file);
+
+	return (w);
+}
