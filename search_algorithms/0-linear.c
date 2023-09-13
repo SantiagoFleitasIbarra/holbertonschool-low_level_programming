588bc8a40db280803937ded9ba448cb27f9ec067
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-09-13 09:19:13 -0700

    fixed bug for betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 # include "search_algos.h"
-/***/
+/**
+ * linear_search - 
+ * @array: first element of the arrangement
+ * @size: array size
+ * @value: value you wish to search for
+ * Return: If it finds the value, it returns the index at which it found it.
+ * If the value is not present in the array or if the array is null, it returns
+ * -1
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
